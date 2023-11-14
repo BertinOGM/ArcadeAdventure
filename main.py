2aca00b454d51239c04a283c4d4b15c40666cf1f
Oli Bertin <72766787+BertinOG@users.noreply.github.com>
2023-11-14 20:55:47 +0000

    Scrolling Menu Errors
    Added sprites for the player to create player class and animations. Tried to implement the main menu by testing if I could stop the scrolling by pressing key. It didnt work

--- main.py
+++ main.py
@@ -19,10 +19,13 @@ tile_size = 50
 keys = pygame.key.get_pressed()
 clock = pygame.time.Clock()
 clock_speed = 30
+scroll = 0
 
 # Surfaces
 player_surf = pygame.image.load("graphics/White_square.png")
 bg = pygame.image.load(os.path.join("graphics/Dungeon_Backgrond_1.png")).convert()
+bg = pygame.transform.scale(bg, (scr_width, scr_height))
+tiles = math.ceil(scr_width / bg.get_width()) + 1
 
 bgx = 0
 bgx2 = bg.get_width()
@@ -31,38 +34,39 @@ bgx2 = bg.get_width()
 player_rect = player_surf.get_rect()
 bg_rect = bg.get_rect()
 
-
 # test world map
-world_data =[
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+world_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
 
 def draw_grid():
     for line in range(0, 20):
-        pygame.draw.line(screen, white, (0, line * tile_size), (scr_width, line* tile_size))
+        pygame.draw.line(screen, white, (0, line * tile_size), (scr_width, line * tile_size))
         pygame.draw.line(screen, white, (line * tile_size, 0), (line * tile_size, scr_height))
-
-
 
 
 # Classes
 
 class Player():
     def __init__(self, x, y):
-        img = pygame.image.load("graphics/White_square.png")
-        img_rect = img.get_rect()
+        img = pygame.image.load("graphics/Sprites/slime-idle-0.png").convert_alpha()
+        self.image = pygame.transform.scale(img, (40, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 
 class World():
     def __init__(self, data):
@@ -89,20 +93,23 @@ class World():
             screen.blit(tile[0], tile[1])
 
 
+player = Player(100, (scr_height - 130))
 world = World(world_data)
 
+
 def main_menu():
-    pygame.draw.rect(bg, bg_rect, midright=0)
-    screen.blit(bg, (bgx2, 0))
+    screen.blit(bg, bg_rect)
+    screen.blit(bg, bg_rect)
     pygame.display.update()
 
 
 # Setting the exit condition
 game_over = False
+menu = True
 
 # While loop for active game
 while not game_over:
-
+    clock.tick(clock_speed)
     # Main loop for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -113,25 +120,28 @@ while not game_over:
             if event.key == pygame.K_h:
                 pygame.image.save(screen, "graphics/screenshot.png")
 
+    while menu:
 
-    clock.tick(clock_speed)
-    main_menu()
-    bgx -= 1.4
-    bgx2 -= 1.4
+        i = 0
+        while i < tiles:
+            screen.blit(bg, (bg.get_width() * i + scroll, 0))
+            i += 1
 
-    if bgx < bg.get_width() * -1:
-        bgx = bg.get_width()
-    if bgx2 < bg.get_width() * -1:
-        bgx2 = bg.get_width()
+        scroll -= 0.08
 
+        if abs(scroll) > bg.get_width():
+            scroll = 0
+        pygame.display.flip()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_i:
+                menu = False
 
     # Screen clearing
-    screen.fill(black)
+    # screen.fill(black)
     # world.draw()
     # draw_grid()
 
     # Screen updating
     pygame.display.flip()
-
 
 pygame.quit()
