5a5b6eaa0e0e961040df2416f74e76df62683f37
Oli Bertin <72766787+BertinOG@users.noreply.github.com>
2023-11-14 12:51:58 +0000

    Graphic update + Screenshot
    Added different graphic to the walls and testing how to screenshot the screen to use in level selector.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.locals import *
 
 # Initializing Pygame
 pygame.init()
@@ -14,6 +13,7 @@ pygame.display.set_caption("Platformer")
 white = (255, 255, 255)
 black = (0, 0, 0)
 tile_size = 50
+keys = pygame.key.get_pressed()
 
 # test world map
 world_data =[
@@ -31,12 +31,24 @@ world_data =[
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, white, (0, line * tile_size), (scr_width, line* tile_size))
+        pygame.draw.line(screen, white, (line * tile_size, 0), (line * tile_size, scr_height))
+
 # Classes
+
+class Player():
+    def __init__(self, x, y):
+        img = pygame.image.load("graphics/White_square.png")
+        img_rect = img.get_rect()
+
 class World():
     def __init__(self, data):
         self.tile_list = []
 
-        border = pygame.image.load("graphics/Dark_blue.png")
+        border = pygame.image.load("graphics/Wall_brick.png")
 
         row_count = 0
         for row in data:
@@ -74,9 +86,15 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
 
+        # Testing screenshot function
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_h:
+                pygame.image.save(screen, "graphics/screenshot.png")
+
     # Screen clearing
     screen.fill(black)
     world.draw()
+    draw_grid()
 
     # Screen updating
     pygame.display.flip()
